4f22ab054d664b9e8d3bd5528e0f39e776e5ea2a
Koha Choji <koha.choji@vistair.com>
2018-11-02 16:33:19 +0000

    Module 4: QuizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -9,10 +11,20 @@ class Quiz extends Component {
   }
 
   render(){
+    const isQuizEnd = ((this.state.quiz_position - 1)
+      === quizData.quiz_questions.length);
+
+    var quizElement;
+    if (isQuizEnd){
+      quizElement = <QuizEnd/>
+    } else {
+      quizElement = <QuizQuestion
+        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+    }
+
     return (
       <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        {quizElement}
       </div>
     );
   }
